2e031c26b843a627afa3261f8d337493abe27147
godwin wilson <godwinarickkat007@gmail.com>
2024-03-17 16:11:08 +0530

    signup and logout added

--- social_book/social_book/settings.py
+++ social_book/social_book/settings.py
@@ -113,3 +113,7 @@ STATICFILES_DIRS= (os.path.join(BASE_DIR, 'static'),)
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
